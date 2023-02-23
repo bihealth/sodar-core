04ccfe53710df166f4502d8a494ba0a61adc7595
Mikko Nieminen <mikko.nieminen@bih-charite.de>
2023-02-23 11:06:57 +0100

    add chromedriver install script (#1127)

--- utility/install_chromedriver.sh
+++ utility/install_chromedriver.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
 
 echo "***********************************************"
-echo "Installing Chrome + Chromedriver for UI Testing"
+echo "Installing Chromedriver for UI Testing         "
 echo "***********************************************"
 
 # Install dependencies
 sudo apt-get update
 sudo apt-get install -y unzip xvfb libxi6 libgconf-2-4
-
-# Install Chrome
-wget -N https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P ~/
-sudo dpkg -i --force-depends ~/google-chrome-stable_current_amd64.deb
-sudo apt-get -f install -y
-rm ~/google-chrome-stable_current_amd64.deb
 
 # Install ChromeDriver
 CHROME_DRIVER_VERSION=$(curl http://chromedriver.storage.googleapis.com/LATEST_RELEASE)
