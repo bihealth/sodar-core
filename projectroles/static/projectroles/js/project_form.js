70e27f729ef2573b17c8a978129fd4554e49f4d9
gromdimon <grom.dima.grom@gmail.com>
2023-03-15 13:44:20 +0100

    ajax

--- projectroles/static/projectroles/js/project_form.js
+++ projectroles/static/projectroles/js/project_form.js
@@ -2,33 +2,10 @@ $(document).ready(function() {
     // Hide settings fields by default
     $('div[id^="div_id_settings"]').hide();
 
-    // Check if it's category/project update and show corresponding fields
-    if ($('#object-type').text() == 'Update Project') {
+    function showOrHideFields(elements) {
         $('div[id^="div_id_settings"]').each(function () {
             var $parentDiv = $(this);
-            var $projectElements = $parentDiv.find('select[data-project-types="project"]')
-                .add($parentDiv.find('input[data-project-types="project"]'))
-                .add($parentDiv.find('textarea[data-project-types="project"]'))
-                .add($parentDiv.find('select[data-project-types="project,category"]'))
-                .add($parentDiv.find('input[data-project-types="project,category"]'))
-                .add($parentDiv.find('textarea[data-project-types="project,category"]'));
-            if ($projectElements.length > 0) {
-                $parentDiv.show();
-            } else {
-                $parentDiv.hide();
-            }
-        });
-    }
-    if ($('#object-type').text() == 'Update Category') {
-        $('div[id^="div_id_settings"]').each(function () {
-            var $parentDiv = $(this);
-            var $categoryElements = $parentDiv.find('select[data-project-types="category"]')
-                    .add($parentDiv.find('input[data-project-types="category"]'))
-                    .add($parentDiv.find('textarea[data-project-types="category"]'))
-                    .add($parentDiv.find('select[data-project-types="project,category"]'))
-                    .add($parentDiv.find('input[data-project-types="project,category"]'))
-                    .add($parentDiv.find('textarea[data-project-types="project,category"]'));
-            if ($categoryElements.length > 0) {
+            if (elements.filter(function() { return $parentDiv.find(this).length }).length > 0) {
                 $parentDiv.show();
             } else {
                 $parentDiv.hide();
@@ -36,38 +13,36 @@ $(document).ready(function() {
         });
     }
 
+    // Check if it's category/project update and show corresponding fields
+    if ($('#object-type').text() == 'Update Project') {
+        var projectElements = $([
+            'select[data-project-types="project"]',
+            'input[data-project-types="project"]',
+            'textarea[data-project-types="project"]',
+            'select[data-project-types="project,category"]',
+            'input[data-project-types="project,category"]',
+            'textarea[data-project-types="project,category"]'
+        ].join(', '));
+        showOrHideFields(projectElements);
+    } else if ($('#object-type').text() == 'Update Category') {
+        var categoryElements = $([
+            'select[data-project-types="category"]',
+            'input[data-project-types="category"]',
+            'textarea[data-project-types="category"]',
+            'select[data-project-types="project,category"]',
+            'input[data-project-types="project,category"]',
+            'textarea[data-project-types="project,category"]'
+        ].join(', '));
+        showOrHideFields(categoryElements);
+    }
+
     // Show settings fields if selected type is project/category in update form
-   $('#div_id_type .form-control').change(function() {
-        if ($('#div_id_type .form-control').val() == 'PROJECT') {
-            $('div[id^="div_id_settings"]').each(function () {
-                var $parentDiv = $(this);
-                var $projectElements = $parentDiv.find('select[data-project-types="project"]')
-                    .add($parentDiv.find('input[data-project-types="project"]'))
-                    .add($parentDiv.find('textarea[data-project-types="project"]'))
-                    .add($parentDiv.find('select[data-project-types="project,category"]'))
-                    .add($parentDiv.find('input[data-project-types="project,category"]'))
-                    .add($parentDiv.find('textarea[data-project-types="project,category"]'));
-                if ($projectElements.length > 0) {
-                    $parentDiv.show();
-                } else {
-                    $parentDiv.hide();
-                }
-            });
-        } else if ($('#div_id_type .form-control').val() == 'CATEGORY') {
-            $('div[id^="div_id_settings"]').each(function () {
-                var $parentDiv = $(this);
-                var $categoryElements = $parentDiv.find('select[data-project-types="category"]')
-                    .add($parentDiv.find('input[data-project-types="category"]'))
-                    .add($parentDiv.find('textarea[data-project-types="category"]'))
-                    .add($parentDiv.find('select[data-project-types="project,category"]'))
-                    .add($parentDiv.find('input[data-project-types="project,category"]'))
-                    .add($parentDiv.find('textarea[data-project-types="project,category"]'));
-                if ($categoryElements.length > 0) {
-                    $parentDiv.show();
-                } else {
-                    $parentDiv.hide();
-                }
-            });
+    $('#div_id_type .form-control').change(function() {
+        var selectedType = $('#div_id_type .form-control').val();
+        if (selectedType == 'PROJECT') {
+            showOrHideFields(projectElements);
+        } else if (selectedType == 'CATEGORY') {
+            showOrHideFields(categoryElements);
         }
     });
 })
