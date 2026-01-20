0f0788b54777baa988f48672318828b3952e0e0f
Federico Marotta <federico.marotta@bih-charite.de>
2026-01-20 13:01:47 +0100

    add tests for username in user dropdown

--- projectroles/tests/test_ui.py
+++ projectroles/tests/test_ui.py
@@ -112,6 +112,77 @@ class TestBaseTemplate(ProjectUITestBase):
         )
         self.assertEqual(modal.is_displayed(), True)
 
+    def test_user_dropdown_username_default(self):
+        """Test visibility for user with setting default (False)"""
+        # username should not be visible
+        self.assertEqual(
+            app_settings.get(
+                APP_NAME, 'dropdown_user_name_display', user=self.user_owner
+            ),
+            False,
+        )
+        self.login_and_redirect(self.user_owner, reverse('home'))
+        with self.assertRaises(NoSuchElementException):
+            self.selenium.find_element(
+                By.ID, 'sodar-navbar-user-dropdown-username'
+            )
+
+    def test_user_dropdown_username_visible(self):
+        """Test visibility for user with setting set True"""
+        # ensure username is visible
+        user = self.user_contributor
+        app_settings.set(
+            APP_NAME, 'dropdown_user_name_display', True, user=user
+        )
+        self.login_and_redirect(user, reverse('home'))
+        username_element = self.selenium.find_element(
+            By.ID, 'sodar-navbar-user-dropdown-username'
+        )
+        self.assertEqual(username_element.text, str(user))
+
+    @override_settings(PROJECTROLES_ALLOW_ANONYMOUS=True)
+    def test_user_dropdown_username_anonymous(self):
+        """Test visibility with anonymous user"""
+        # username should not be visible
+        self.project.set_public_access(self.role_guest)
+        self.selenium.get(self.build_selenium_url(reverse('home')))
+        WebDriverWait(self.selenium, self.wait_time).until(
+            ec.presence_of_element_located(
+                (By.CLASS_NAME, 'sodar-pr-project-list-item')
+            )
+        )
+        with self.assertRaises(NoSuchElementException):
+            self.selenium.find_element(
+                By.ID, 'sodar-navbar-user-dropdown-username'
+            )
+
+    def test_user_dropdown_username_superuser(self):
+        """Test visibility with superuser"""
+        # username should not be visible
+        self.login_and_redirect(self.superuser, reverse('home'))
+        with self.assertRaises(NoSuchElementException):
+            self.selenium.find_element(
+                By.ID, 'sodar-navbar-user-dropdown-username'
+            )
+
+    def test_user_dropdown_username_truncated(self):
+        """Test visibility and truncation for user with long name"""
+        long_name_user = self.make_user('long_name_which_should_be_truncated')
+        app_settings.set(
+            APP_NAME, 'dropdown_user_name_display', True, user=long_name_user
+        )
+        self.login_and_redirect(long_name_user, reverse('home'))
+        username_element = self.selenium.find_element(
+            By.ID, 'sodar-navbar-user-dropdown-username'
+        )
+        # Based on https://medium.com/@dave_go/testing-text-truncation-effective-strategies-2a589841a22b
+        # We compare offsetWidth (which should be equal to the max-width css
+        # property) to scrollWidth (which is the total width)
+        self.assertTrue(
+            username_element.get_attribute("offsetWidth")
+            < username_element.get_attribute("scrollWidth")
+        )
+
 
 class TestHomeView(ProjectUITestBase):
     """Tests for the home view and project list UI"""
