5a5305b4c1f72b3e223bb65a738c661b376bf5c7
Federico Marotta <federico.marotta@bih-charite.de>
2026-02-25 14:31:59 +0100

    fix type hint and docstring for keywords argument (#1894)

--- projectroles/plugins.py
+++ projectroles/plugins.py
@@ -429,7 +429,7 @@ class ProjectAppPluginPoint(PluginPoint):
         search_terms: list[str],
         user: User,
         search_type: Optional[str] = None,
-        keywords: Optional[list[str]] = None,
+        keywords: Optional[dict] = None,
     ) -> list['PluginSearchResult']:
         """
         Return app items based on one or more search terms, user, optional type
@@ -439,7 +439,7 @@ class ProjectAppPluginPoint(PluginPoint):
                              (list of strings)
         :param user: User object for user initiating the search
         :param search_type: String
-        :param keywords: List (optional)
+        :param keywords: Dictionary of key/value pairs (optional)
         :return: List of PluginSearchResult objects
         """
         # TODO: If implemented, also implement display of results in the app's
