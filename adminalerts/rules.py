b2e3323cba59e40db2f014a3c1798cab0b743576
Federico Marotta <federico.marotta@bih-charite.de>
2026-03-31 10:25:33 +0200

    update permission for adminalerts.view_list (#1916)

--- adminalerts/rules.py
+++ adminalerts/rules.py
@@ -22,7 +22,7 @@ import rules
 rules.add_perm('adminalerts.view_alert', rules.is_authenticated)
 
 # Allow viewing alert list
-rules.add_perm('adminalerts.view_list', rules.is_superuser)
+rules.add_perm('adminalerts.view_list', rules.is_authenticated)
 
 # Allow creation of alerts
 rules.add_perm('adminalerts.create_alert', rules.is_superuser)
